e582304637ba702afc4eb14f8f8963f9358efe6e
María Cecilia Cotzajay López <3033707770108@ingenieria.usac.edu.gt>
2024-06-07 10:16:26 -0600

    Modelo entidar relacion(documentacion) y Script BD(backend)

---
+++ backend/database/BD.sql
@@ -0,0 +1,78 @@
+
+CREATE TABLE Alquiler
+(
+  id_alquiler      INT      NOT NULL,
+  fecha_alquiler   DATETIME NOT NULL,
+  fecha_devolucion DATETIME NOT NULL,
+  multa            DOUBLE   NOT NULL,
+  id_user          INT      NOT NULL,
+  id_pelicula      TEXT     NOT NULL,
+  PRIMARY KEY (id_alquiler)
+);
+
+CREATE TABLE Comentario
+(
+  id_comentario INT  NOT NULL,
+  contenido     TEXT NOT NULL,
+  id_user       INT  NOT NULL,
+  id_pelicula   TEXT NOT NULL,
+  PRIMARY KEY (id_comentario)
+);
+
+CREATE TABLE Genero
+(
+  id_genero INT  NOT NULL,
+  nombre    TEXT NOT NULL,
+  PRIMARY KEY (id_genero)
+);
+
+CREATE TABLE Pelicula
+(
+  id_pelicula     TEXT   NOT NULL,
+  titulo          TEXT   NOT NULL,
+  sinopsis        TEXT   NOT NULL,
+  precio_alquiler DOUBLE NOT NULL,
+  director        TEXT   NOT NULL,
+  anio_estreno    YEAR   NOT NULL,
+  duracion        TIME   NOT NULL,
+  imagen          TEXT   NOT NULL,
+  id_genero       INT    NOT NULL,
+  PRIMARY KEY (id_pelicula)
+);
+
+CREATE TABLE User
+(
+  id_user          INT  NOT NULL,
+  nombre           TEXT NOT NULL,
+  apellido         TEXT NOT NULL,
+  genero           CHAR NOT NULL COMMENT ''f' o 'm'',
+  correo           TEXT NOT NULL,
+  contrasenia      TEXT NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  PRIMARY KEY (id_user)
+);
+
+ALTER TABLE Comentario
+  ADD CONSTRAINT FK_User_TO_Comentario
+    FOREIGN KEY (id_user)
+    REFERENCES User (id_user);
+
+ALTER TABLE Comentario
+  ADD CONSTRAINT FK_Pelicula_TO_Comentario
+    FOREIGN KEY (id_pelicula)
+    REFERENCES Pelicula (id_pelicula);
+
+ALTER TABLE Alquiler
+  ADD CONSTRAINT FK_User_TO_Alquiler
+    FOREIGN KEY (id_user)
+    REFERENCES User (id_user);
+
+ALTER TABLE Pelicula
+  ADD CONSTRAINT FK_Genero_TO_Pelicula
+    FOREIGN KEY (id_genero)
+    REFERENCES Genero (id_genero);
+
+ALTER TABLE Alquiler
+  ADD CONSTRAINT FK_Pelicula_TO_Alquiler
+    FOREIGN KEY (id_pelicula)
+    REFERENCES Pelicula (id_pelicula);
