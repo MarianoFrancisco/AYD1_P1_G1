c907d8d6dbcbcb0505dfc3e6613271d2758fa0c4
María Cecilia Cotzajay López <3033707770108@ingenieria.usac.edu.gt>
2024-06-07 18:49:35 -0600

    Corrección ERD y Script BD
    Corregidos para usar en MySQL

--- backend/database/BD.sql
+++ backend/database/BD.sql
@@ -1,78 +1,57 @@
-
-CREATE TABLE Alquiler
+CREATE TABLE User
 (
-  id_alquiler      INT      NOT NULL,
-  fecha_alquiler   DATETIME NOT NULL,
-  fecha_devolucion DATETIME NOT NULL,
-  multa            DOUBLE   NOT NULL,
-  id_user          INT      NOT NULL,
-  id_pelicula      TEXT     NOT NULL,
-  PRIMARY KEY (id_alquiler)
-);
-
-CREATE TABLE Comentario
-(
-  id_comentario INT  NOT NULL,
-  contenido     TEXT NOT NULL,
-  id_user       INT  NOT NULL,
-  id_pelicula   TEXT NOT NULL,
-  PRIMARY KEY (id_comentario)
+  id_user          INT AUTO_INCREMENT NOT NULL,
+  nombre           VARCHAR(65) NOT NULL,
+  apellido         VARCHAR(65) NOT NULL,
+  genero           CHAR(1) NOT NULL COMMENT "'F' o 'M'",
+  correo           VARCHAR(65) NOT NULL UNIQUE,
+  contrasenia      VARCHAR(65) NOT NULL,
+  fecha_nacimiento DATE NOT NULL,
+  PRIMARY KEY (id_user)
 );
 
 CREATE TABLE Genero
 (
-  id_genero INT  NOT NULL,
-  nombre    TEXT NOT NULL,
+  id_genero INT AUTO_INCREMENT NOT NULL,
+  nombre    VARCHAR(65) NOT NULL,
   PRIMARY KEY (id_genero)
 );
 
 CREATE TABLE Pelicula
 (
-  id_pelicula     TEXT   NOT NULL,
-  titulo          TEXT   NOT NULL,
-  sinopsis        TEXT   NOT NULL,
-  precio_alquiler DOUBLE NOT NULL,
-  director        TEXT   NOT NULL,
-  anio_estreno    YEAR   NOT NULL,
-  duracion        TIME   NOT NULL,
-  imagen          TEXT   NOT NULL,
-  id_genero       INT    NOT NULL,
-  PRIMARY KEY (id_pelicula)
+  id_pelicula     INT AUTO_INCREMENT NOT NULL,
+  titulo          VARCHAR(65) NOT NULL,
+  sinopsis        VARCHAR(255) NOT NULL,
+  precio_alquiler DECIMAL(10, 2) NOT NULL,
+  director        VARCHAR(65) NOT NULL,
+  anio_estreno    YEAR NOT NULL,
+  duracion        TIME NOT NULL,
+  imagen          VARCHAR(255) NOT NULL,
+  id_genero       INT NOT NULL,
+  PRIMARY KEY (id_pelicula),
+  FOREIGN KEY (id_genero) REFERENCES Genero (id_genero)
 );
 
-CREATE TABLE User
+CREATE TABLE Alquiler
 (
-  id_user          INT  NOT NULL,
-  nombre           TEXT NOT NULL,
-  apellido         TEXT NOT NULL,
-  genero           CHAR NOT NULL COMMENT ''f' o 'm'',
-  correo           TEXT NOT NULL,
-  contrasenia      TEXT NOT NULL,
-  fecha_nacimiento DATE NOT NULL,
-  PRIMARY KEY (id_user)
+  id_alquiler      INT AUTO_INCREMENT NOT NULL,
+  fecha_alquiler   DATETIME NOT NULL,
+  fecha_devolucion DATETIME NOT NULL,
+  multa            DECIMAL(10, 2) NOT NULL,
+  id_user          INT NOT NULL,
+  id_pelicula      INT NOT NULL,
+  PRIMARY KEY (id_alquiler),
+  FOREIGN KEY (id_user) REFERENCES User (id_user),
+  FOREIGN KEY (id_pelicula) REFERENCES Pelicula (id_pelicula)
 );
 
-ALTER TABLE Comentario
-  ADD CONSTRAINT FK_User_TO_Comentario
-    FOREIGN KEY (id_user)
-    REFERENCES User (id_user);
-
-ALTER TABLE Comentario
-  ADD CONSTRAINT FK_Pelicula_TO_Comentario
-    FOREIGN KEY (id_pelicula)
-    REFERENCES Pelicula (id_pelicula);
-
-ALTER TABLE Alquiler
-  ADD CONSTRAINT FK_User_TO_Alquiler
-    FOREIGN KEY (id_user)
-    REFERENCES User (id_user);
-
-ALTER TABLE Pelicula
-  ADD CONSTRAINT FK_Genero_TO_Pelicula
-    FOREIGN KEY (id_genero)
-    REFERENCES Genero (id_genero);
-
-ALTER TABLE Alquiler
-  ADD CONSTRAINT FK_Pelicula_TO_Alquiler
-    FOREIGN KEY (id_pelicula)
-    REFERENCES Pelicula (id_pelicula);
+CREATE TABLE Comentario
+(
+  id_comentario INT AUTO_INCREMENT NOT NULL,
+  contenido     VARCHAR(255) NOT NULL,
+  id_user       INT NOT NULL,
+  id_pelicula   INT NOT NULL,
+  PRIMARY KEY (id_comentario),
+  FOREIGN KEY (id_user) REFERENCES User (id_user),
+  FOREIGN KEY (id_pelicula) REFERENCES Pelicula (id_pelicula)
+);
