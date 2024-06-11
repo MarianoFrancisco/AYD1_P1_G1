559c9f067ca5db80f920cc2e51b9c5a523dcc97e
Mariano Camposeco <mariano1941@outlook.es>
2024-06-10 21:22:50 -0600

    add new endpoints for comentario

--- backend/app/routes/comentario.routes.js
+++ backend/app/routes/comentario.routes.js
@@ -8,9 +8,15 @@ const comentarioController = require('../controllers/comentarioController');
 const api = '/api'
 const comentario = api + '/comentarios'
 const comentario_id_pelicula = comentario + '/:id_pelicula'
+const comentario_pelicula_usuario = comentario_id_pelicula + '/usuario/:id_user'
+const comentario_individual = comentario + '/individual/:id_comentario'
 
 router.get(comentario_id_pelicula, comentarioController.obtenerComentariosPorPelicula);
 
+router.get(comentario_pelicula_usuario, comentarioController.obtenerComentariosPorPeliculaUsuario);
+
 router.post(comentario, comentarioController.agregarComentario);
+
+router.delete(comentario_individual, comentarioController.eliminarComentario);
 
 module.exports = router;
